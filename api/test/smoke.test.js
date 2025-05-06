1d762ca885f53222f62a9cb90c0073bb345b2fef
likithashreebc <155794279+likithashreebc@users.noreply.github.com>
2025-05-06 15:35:10 +0530

    Update smoke.test.js to import app from index.js

--- api/test/smoke.test.js
+++ api/test/smoke.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../server');
+const app = require('../index');
 
 describe('Smoke Test', () => {
   it('Server should start', async () => {
